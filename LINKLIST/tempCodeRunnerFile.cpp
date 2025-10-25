ae87467b0dc0a6eb151ec45c59edab2152c619b9
jayamehra077-ui <jayamehra077@gmail.com>
2025-10-25 23:41:57 +0530

    DSA

--- LINKLIST/tempCodeRunnerFile.cpp
+++ LINKLIST/tempCodeRunnerFile.cpp
@@ -21,7 +21,7 @@ class list
     head=tail=NULL;
 
  }
- void push_front(int val)
+ void push_back(int val)
  {
 node* newnode=new node(val);//data stors permanently
 //node newnode(val); "data erase after func calling"
@@ -34,22 +34,27 @@ if(head==NULL)
     return;
 }
 else{
-    newnode->next=head;
-    head=newnode;
+    tail->next=newnode;
+    tail=newnode;
 }
 }
-void pop_front()
+void pop_back()
 {
     if(head==NULL)
     {
-        cout<<"link list is empty!!!!!\n";
+        cout<<"link list is empty";
         return;
     }
-    else{
-        node* temp=head;
-        head=head->next;
-        temp->next=NULL;
-        delete temp;
+    else
+    {
+      node* temp=head;
+      while(temp->next!=tail)
+      {
+       temp=temp->next;
+      }
+      temp->next=NULL;
+      delete tail;
+      tail=temp;
     }
 }
  void printll()
@@ -66,11 +71,11 @@ void pop_front()
 };
 int main(){
 list ll;
-ll.push_front(2);
-ll.push_front(3);
-ll.push_front(4);
-ll.push_front(5);
-ll.pop_front();
+ll.push_back(2);
+ll.push_back(3);
+ll.push_back(4);
+ll.pop_back();
 ll.printll();
+ll.pop_back();
 return 0;
 }
